aeadf351dbc3707774affaec21428293f469c52e
zaheerahmed8845 <zaheerahmed8845@gmail.com>
2023-03-24 14:38:03 +0530

    builder pattern

---
+++ src/main/java/com/designpattern/builder/builder1/UserDTO.java
@@ -0,0 +1,11 @@
+package com.designpattern.builder.builder1;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
